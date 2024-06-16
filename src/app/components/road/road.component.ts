c364518552481ba5391624e46ee53d033a210d0e
VladR <vladi.rolan@gmail.com>
2024-06-16 22:55:46 +0300

    working 65%

--- src/app/components/road/road.component.ts
+++ src/app/components/road/road.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input} from '@angular/core';
-import { RouterService, RoutingType, travel } from 'src/app/services/router.service'
+import { Component, OnInit, Input } from '@angular/core';
+import { RouterService, RoutingType, travel } from 'src/app/services/router.service';
 
 @Component({
   selector: 'app-road',
@@ -7,16 +7,17 @@ import { RouterService, RoutingType, travel } from 'src/app/services/router.serv
   styleUrls: ['./road.component.scss']
 })
 export class RoadComponent implements OnInit {
-  @Input() selectedStart?: RoutingType
-  @Input() selectedFinish!: RoutingType
-  openTable: boolean = false
-  route: any[] = []
+  @Input() selectedStart?: RoutingType;
+  @Input() selectedFinish!: RoutingType;
+  openTable: boolean = false;
+  route: any[] = [];
 
-  constructor(public routerService: RouterService) {this.routerService.jsonRouter = []}
-
+  constructor(public routerService: RouterService) {
+    this.routerService.jsonRouter = [];
+  }
 
   ngOnInit(): void {
-    this.routerService.getJsonRouter()
+    this.routerService.getJsonRouter();
   }
 
   getRoad(): void {
@@ -24,27 +25,46 @@ export class RoadComponent implements OnInit {
       return console.log('Выберите города');
     }
 
-    let filtered = this.selectedStart.travel.filter((city: travel) => {
-      return city.to === this.selectedFinish.from
-    })
+    this.route = [];
+    this.findRoutes(this.selectedStart.from, this.selectedFinish.from, []);
 
-    for(let get of filtered){
-      this.route.push({
-        from: this.selectedStart.from,
-        to: this.selectedFinish.from,
-        price: get.price,
-        time: get.time
-      })
-      console.log(`Цена: ${get.price} рублей, Время: ${get.time}`)
-    }
-    this.openTable = !this.openTable
-    if(this.openTable === false){
-      this.route = []
-    } else if(this.route.length === 0){
-      this.openTable = false
-      alert('Маршрут не найден')
+    this.openTable = this.route.length > 0;
+    if (!this.openTable) {
+      alert('Маршрут не найден');
     }
 
-    console.log(this.route)
+    console.log(this.route);
+  }
+
+  findRoutes(currentCity: string, targetCity: string, visited: any[]): boolean {
+    if (currentCity === targetCity) {
+      return true;
+    }
+
+    const currentRoutes = this.routerService.jsonRouter.find((route: RoutingType) => route.from === currentCity);
+    if (!currentRoutes) {
+      return false;
+    }
+
+    for (let travel of currentRoutes.travel) {
+      if (!visited.includes(travel.to)) {
+        visited.push(travel.to);
+        this.route.push({
+          from: currentCity,
+          to: travel.to,
+          price: travel.price,
+          time: travel.time
+        });
+
+        if (this.findRoutes(travel.to, targetCity, visited)) {
+          return true;
+        }
+
+        this.route.pop();
+        visited.pop();
+      }
+    }
+
+    return false;
   }
 }
